28911eb3efda6a5bf7c1587118cf36918a2bf4e0
Alejandro Barboza <barbozaalejandro@outlook.com.ar>
2016-03-25 05:15:04 -0300

    Update funciones.c

--- factorial/funciones.c
+++ factorial/funciones.c
@@ -1,3 +1,8 @@
+/** \brief recibe un número y calcula su factorial usando recursividad
+ *
+ * \return la 1er sentencia inicia la recursividad multiplicando x por la propia funcion de x-1 hasta que x valga 1
+ * \return la 2da sentencia al devolver 1 a la funcion, inicia el camino inverso de la anterior, devolviendo asi los valores para resolver todos los calculos de x * factorial(x-1)
+ */
 int factorial(int x)
 {
     if(x>1)
